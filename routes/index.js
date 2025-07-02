6c4c3faf8db15a8abee0051ee745d1cd006731b2
leonardotomasini <leonardotomasini20@gmail.com>
2025-07-02 08:48:12 -0300

    primeiras alterações, criações das views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Página inicial
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Página Inicial' });
+});
+
+// Página de cadastro
+router.get('/cadastro', function(req, res) {
+  res.render('cadastro', { title: 'Cadastro' });
+});
+
+// Página de login
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login' });
+});
+
+// Página principal após login
+router.get('/pagina', function(req, res) {
+  res.render('pagina', { title: 'Página Principal' });
+});
+
+// Página de erro (opcional, normalmente usada para tratamento de erros)
+router.get('/erro', function(req, res) {
+  res.render('error', { message: 'Erro personalizado', error: {} });
 });
 
 module.exports = router;
