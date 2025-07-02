364eafcff2312de6ecb271baef66f61fb6c72110
Thierry <thdossant12@gmail.com>
2025-07-02 11:02:27 -0300

    Correção de rotas e páginas

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 // Página inicial
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Página Inicial' });
-});
-
-// Página de cadastro
-router.get('/cadastro', function(req, res) {
-  res.render('cadastro', { title: 'Cadastro' });
+  res.render('cadastro');
 });
 
 // Página de login
@@ -16,10 +11,6 @@ router.get('/login', function(req, res) {
   res.render('login', { title: 'Login' });
 });
 
-// Página principal após login
-router.get('/pagina', function(req, res) {
-  res.render('pagina', { title: 'Página Principal' });
-});
 
 // Página de erro (opcional, normalmente usada para tratamento de erros)
 router.get('/erro', function(req, res) {
