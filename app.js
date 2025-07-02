b3c68222133a7b61eea64a0608f0c6c5c0ac1f8e
JoaoZanelato <jgbzanelato@gmail.com>
2025-07-02 11:39:17 -0300

    meudeus algm me mata

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session'); //  Adicione esta linha
 
 var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users'); // Você pode remover se não usar
 
 var app = express();
 
@@ -12,21 +14,29 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// --- CONFIGURAÇÃO DO MIDDLEWARE ---
+// A ordem aqui é importante!
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//  Configure o express-session AQUI
+app.use(session({
+  secret: 'sua-chave-secreta-super-dificil', // TROQUE ISTO POR UMA CHAVE REAL E SEGURA
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Para desenvolvimento. Em produção (com HTTPS), use 'true'.
+}));
+
+// Servir arquivos estáticos (CSS, imagens)
 app.use(express.static(path.join(__dirname, 'public')));
 
+//  Configure as rotas DEPOIS da sessão
 app.use('/', indexRouter);
+// app.use('/users', usersRouter); // Você pode remover se não usar
 
-var session = require('express-session');
-
-app.use(session({
-  secret: 'sua-chave-secreta-aqui', // Troque por uma chave secreta real
-  resave: false,
-  saveUninitialized: true
-}));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
